4a8c86f4790d2b36caca011aa6f1e3ef542b93cb
laz4rz <boronskimikolaj@gmail.com>
2024-09-10 02:37:33 +0200

    correctly multiply 2x2 matrices, some idx error with 3x3

--- matrix.py
+++ matrix.py
@@ -1,10 +1,12 @@
 class Matrix:
     # 3d implemented with strided representation in row-major order
+    # matrices are treated as depth-number of stacked 2d matrices
     def __init__(self, d=1, h=1, w=1, data=None):
         self.shape = (d, h, w)
+        self.size = d * h * w
         self.stride = (h * w, w, 1)
         if data is not None:
-            assert len(data) == d * h * w
+            assert len(data) == self.size
             self.data = data
         else:
             self.data = [0 for _ in range(h) for _ in range(d) for _ in range(w)]
@@ -26,24 +28,98 @@ class Matrix:
             
         temp = []
         print("stride:", self.stride)
-        for d in range(idx[2][0], idx[2][1]):
-            for h in range(idx[0][0], idx[0][1]):
-                for w in range(idx[1][0], idx[1][1]):
-                    print("  h, w, d:", h, w, d)
-                    strided_idx = d * self.stride[0] + h * self.stride[1] + w * self.stride[2]
-                    print("  strided:",strided_idx)
+        for d in range(idx[0][0], idx[0][1]):
+            for h in range(idx[1][0], idx[1][1]):
+                for w in range(idx[2][0], idx[2][1]):
+                    print("  d, h, w:", d, h, w)
+                    strided_idx = self._get_strided(d, h, w)
                     temp.append(self.data[strided_idx])
 
-        return Matrix(idx[2][1] - idx[2][0], idx[0][1] - idx[0][0], idx[1][1] - idx[1][0], temp)
+        if len(temp) > 1:
+            return Matrix(idx[2][1] - idx[2][0], idx[0][1] - idx[0][0], idx[1][1] - idx[1][0], temp)
+        else:
+            return temp[0]
+
+    def _get_strided(self, d, h, w):
+        strided_idx = d * self.stride[0] + h * self.stride[1] + w * self.stride[2]
+        print("  strided:", strided_idx)
+        return strided_idx
     
+    def __setitem__(self, idx, item):
+        d, h, w = idx
+        strided_idx = self._get_strided(d, h, w)
+        self.data[strided_idx] = item.data[0] if isinstance(item, Matrix) and item.size == 1 else item
+
+    def __matmul__(self, other):
+        assert self.shape[2] == other.shape[1], f"incompatible shapes for matmul: {self.shape[2]} != {other.shape[1]}" 
+        assert self.shape[0] == other.shape[0], f"incompatible depth shapes for matmul: {self.shape[0]} != {other.shape[0]}"
+
+        temp = Matrix(self.shape[0], self.shape[1], other.shape[2])
+        for d in range(self.shape[0]):
+            for h in range(self.shape[1]):
+                for w in range(self.shape[2]):
+                    s = 0
+                    for i in range(self.shape[2]):
+                        print(d, h, w, i)
+                        s += self[d, h, i] * other[d, i, w]
+                    
+                    temp[d, h, w] = s
+
+        return temp
+    
+    def __mul__(self, other):
+        assert self.shape == other.shape, f"can only scalar multiply same sized matrices: {self.shape} != {other.shape}" 
+
+        temp = Matrix(self.shape[0], self.shape[1], self.shape[2])
+        for d in range(self.shape[0]):
+            for h in range(self.shape[1]):
+                for w in range(self.shape[2]):
+                    temp[d, h, w] = self[d, h, w] * other[d, h, w]
+
+        return temp 
+    
+    def __div__(self, other):
+        assert self.shape == other.shape, f"can only scalar multiply same sized matrices: {self.shape} != {other.shape}" 
+
+        temp = Matrix(self.shape[0], self.shape[1], self.shape[2])
+        for d in range(self.shape[0]):
+            for h in range(self.shape[1]):
+                for w in range(self.shape[2]):
+                    temp[d, h, w] = self[d, h, w] / other[d, h, w]
+
+        return temp 
+    
+    def __add__(self, other):
+        assert self.shape == other.shape, f"can only scalar multiply same sized matrices: {self.shape} != {other.shape}" 
+
+        temp = Matrix(self.shape[0], self.shape[1], self.shape[2])
+        for d in range(self.shape[0]):
+            for h in range(self.shape[1]):
+                for w in range(self.shape[2]):
+                    temp[d, h, w] = self[d, h, w] + other[d, h, w]
+
+        return temp 
+    
+    def __sub__(self, other):
+        assert self.shape == other.shape, f"can only scalar multiply same sized matrices: {self.shape} != {other.shape}" 
+
+        temp = Matrix(self.shape[0], self.shape[1], self.shape[2])
+        for d in range(self.shape[0]):
+            for h in range(self.shape[1]):
+                for w in range(self.shape[2]):
+                    temp[d, h, w] = self[d, h, w] - other[d, h, w]
+
+        return temp 
+
     def __str__(self):
         return str(self.data)
 
     def __repr__(self):
         return "Matrix("+str(self.__dict__)+")"
     
-m = Matrix(2, 2)
-print(m.__repr__())
-m2 = m[:, :1]
-print(m2.__repr__())
-m2[:1, :, :]
+m = Matrix(1, 2, 2)
+print()
+print(m @ m)
+print()
+# m2 = Matrix(1, 3, 3, [i for i in range(1, 10)])
+# print(m2 @ m2)
